af8ac7f15479866bc816c83407329017ebf21c95
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-06-11 00:28:20 +1000

    SSL env fix v2

--- utils/databasepg.js
+++ utils/databasepg.js
@@ -2,14 +2,17 @@ const Client = require('pg').Client;
 
 require('dotenv').config();
 
-const client = new Client({
+let secret = {
   host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
   port: process.env.POSTGRES_PORT,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  ssl: process.env.POSTGRES_SSL
-});
+}
+
+if(process.env.POSTGRES_SSL) secret.ssl = process.env.POSTGRES_SSL
+
+const client = new Client(secret);
 
 
 module.exports = client;
